b0cacfe83010ee7055a316a33c3125e46380c263
Rinat <oblivion920@yandex.ru>
2021-09-06 03:58:27 +0300

    initial commit

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -29,9 +29,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Application definition
